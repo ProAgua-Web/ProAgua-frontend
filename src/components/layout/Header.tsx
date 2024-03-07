0457e5e32f92294aee6add155ff0a56f5cf22ff9
Thiago Coelho <thiago.coelho158@gmail.com>
2024-03-06 21:26:50 -0300

    feat: adicionar tag header ao componente Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -54,36 +54,40 @@ export default function(props: {
     collapsed: Boolean
 }) {
     return (
-        <div 
-            id="header-bar"
-            className="fixed flex z-10 w-full items-center h-[clamp(50px, 8vh, 100px)] bg-primary-500 text-white-100"
-        >
-            <button 
-                id="menu-button"
-                onClick={ props.expand }
-                className="h-20 w-20 bg-primary-500 hover:bg-primary-600"
-            >l
-                <i className="bi bi-list header-icon"></i>
-            </button>
-            
-            <div
-                id="header-content"
-                className="w-full flex justify-center"
+        <header>
+            <div 
+                id="header-bar"
+                className="fixed flex z-10 w-full items-center h-[clamp(50px, 8vh, 100px)] bg-primary-500 text-white-100"
             >
-                <div id="proagua-logo" className="w-fit flex items-center gap-4">
-                    <Image
-                        src={ ProAguaLogo } 
-                        className="max-h-10"
-                        alt="Logo do projeto"
-                    />
-                    <a 
-                        href="/"
-                        className="text-2xl font-semibold text-white-100"
-                    >
-                        ProÁgua
-                    </a>
+                <button 
+                    id="menu-button"
+                    onClick={ props.expand }
+                    className="h-20 w-20 bg-primary-500 hover:bg-primary-600"
+                >
+                    -
+                </button>
+                <i className="bi bi-list header-icon"></i>
+                
+                <div
+                    id="header-content"
+                    className="w-full flex justify-center"
+                >
+                    <div id="proagua-logo" className="w-fit flex items-center gap-4">
+                        <Image
+                            src={ ProAguaLogo } 
+                            className="max-h-10"
+                            alt="Logo do projeto"
+                        />
+                        <a 
+                            href="/"
+                            className="text-2xl font-semibold text-white-100"
+                        >
+                            ProÁgua
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </header>
+        
     )
 }
