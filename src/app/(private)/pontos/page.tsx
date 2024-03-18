987470da81a50bfe7c77ae01ac59a71111a0b49d
Thiago Coelho <thiago.coelho158@gmail.com>
2024-03-17 23:22:05 -0300

    feat: adicionar botão de criação de pontos

--- src/app/(private)/pontos/page.tsx
+++ src/app/(private)/pontos/page.tsx
@@ -12,6 +12,7 @@ export default function Pontos() {
       <h2 className="text-3xl text-[#525252]">Pontos de Coleta</h2>
       <div className="flex w-full flex-col items-center">
         <Filters />
+        <a href="/pontos/criar" className="p-4 bg-primary-500 text-white font-semibold rounded-md hover:bg-primary-600">+ Adicionar ponto</a>
         <div
           id="result-list"
           className="grid w-full grid-cols-[repeat(auto-fill,minmax(260px,1fr))] justify-center gap-8"
