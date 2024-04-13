bfd4f6b9d1912151e1e98d443583a5c34ef3bb72
Thiago Coelho <thiago.coelho158@gmail.com>
2024-04-13 13:58:49 -0300

    feat: create point status dashboard

--- src/app/(public)/pontos/[id_ponto]/page.tsx
+++ src/app/(public)/pontos/[id_ponto]/page.tsx
@@ -3,6 +3,49 @@ import { Coleta, Ponto } from "@/utils/api_consumer";
 import { API_BASE_URL } from "@/utils/config";
 
 
+function PointDashboard(props: {coleta: Coleta}) {
+    const { coleta } = props;
+    const bg_color = coleta.status ? "bg-green-200" : "bg-orange-200";
+    return (
+        <div className="flex gap-8 p-8">
+            <div className={"rounded-lg p-4 shadow " + bg_color}>
+                <h1>Coliformes totais</h1>
+                <p>
+                    {coleta.coliformes_totais ? "Presença" : "Ausência"}
+                </p>
+            </div>
+            
+            <div className={"rounded-lg p-4 shadow " + bg_color}>
+                <h1>Escherichia coli</h1>
+                <p>
+                    {coleta.escherichia ? "Presença" : "Ausência"}
+                </p>
+            </div>
+            
+            <div className={"rounded-lg p-4 shadow " + bg_color}>
+                <h1>Turbidez</h1>
+                <p>
+                    {coleta.turbidez} uT
+                </p>
+            </div>
+            
+            <div className={"rounded-lg p-4 shadow " + bg_color}>
+                <h1>Cloro residual livre</h1>
+                <p>
+                    {coleta.cloro_residual_livre} mg/L
+                </p>
+            </div>
+
+            <div className={"rounded-lg p-4 shadow " + bg_color}>
+                <h1>Temperatura</h1>
+                <p>
+                    {coleta.temperatura} ºC
+                </p>
+            </div>
+        </div>
+    )
+}
+
 export default async function Page(props: { 
     params: { id_ponto:number }
 }) {
@@ -17,29 +60,30 @@ export default async function Page(props: {
     return (
         <>
             <Header />
-            <div className="pt-[calc(clamp(50px,8vh,100px)+2rem)]">
-                API URL: {API_BASE_URL} <br />
-                Coletas: {coletas.length}
-
-                ID ponto: {ponto.id}<br />
-                Prédio: {ponto.edificacao.codigo} - {ponto.edificacao.nome} {ponto.edificacao.campus}
-                <br />
-                Ambiente: {ponto.ambiente}<br />
-                Status: {ponto.status ? "Em conformidade" : "Não conforme"}<br />
+            <div className="pt-[calc(clamp(50px,8vh,100px)+2rem)] px-8">
+                <div className="p-4 rounded border border-neutral-300 shadow">
+                    ID ponto: {ponto.id}<br />
+                    Prédio: {ponto.edificacao.codigo} - {ponto.edificacao.nome} {ponto.edificacao.campus}
+                    <br />
+                    Ambiente: {ponto.ambiente}<br />
+                    Status: {ponto.status ? "Em conformidade" : "Não conforme"}<br />
+                </div>
+                <div>
+                    {/* {coleta && 
+                        <>
+                            id: {coleta.id}; 
+                            cloro: {coleta.cloro_residual_livre}; 
+                            cor: {coleta.cor}; 
+                            data: {coleta.data}; 
+                            coliformes: {coleta.coliformes_totais ? "Presença" : "Ausência"}; 
+                            escherichia: {coleta.escherichia ? "Presença" : "Ausência"} ; 
+                            status: {coleta.status.status ? "Em conformidade" : "Não conforme"};
+                        </>    
+                    } */}
+                    {coleta && <PointDashboard coleta={coleta} />}
+                </div>
             </div>
-            <div>
-                {coleta && 
-                <>
-                    id: {coleta.id}; 
-                    cloro: {coleta.cloro_residual_livre}; 
-                    cor: {coleta.cor}; 
-                    data: {coleta.data}; 
-                    coliformes: {coleta.coliformes_totais ? "Presença" : "Ausência"}; 
-                    escherichia: {coleta.escherichia ? "Presença" : "Ausência"} ; 
-                    status: {coleta.status.status ? "Em conformidade" : "Não conforme"};
-                </>    
-                }
-            </div>
+            
         </>
     )
 }
