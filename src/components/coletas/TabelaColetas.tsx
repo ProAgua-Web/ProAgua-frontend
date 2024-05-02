73ae99c77a78fddb403395e1cf5feb7eca712097
Thiago Coelho <thiago.coelho158@gmail.com>
2024-05-02 11:24:16 -0300

    feat: atualizar estilização de componente TabelaColetas

--- src/components/coletas/TabelaColetas.tsx
+++ src/components/coletas/TabelaColetas.tsx
@@ -1,45 +1,45 @@
-import { Coleta } from "@/utils/api_consumer";
+import { Coleta } from "@/utils/types";
 
 export default function TableColetas(props: {coletas: Coleta[]}) {
     const { coletas } = props;
     return (
         <>
-            <table className="max-w-full overflow-x-auto border border-black mb-8 last:mb-0">
+            <table className="max-w-full overflow-x-auto border border-slate-500 mb-8 last:mb-0">
                 <thead>
-                    <tr className="bg-sky-900 text-white">
-                        <th className="px-4 py-2">ID</th>
-                        <th className="px-4 py-2">Ordem</th>
-                        <th className="px-4 py-2">Temperatura</th>
-                        <th className="px-4 py-2">Cloro residual livre</th>
-                        <th className="px-4 py-2">Turbidez</th>
-                        <th className="px-4 py-2">Coliformes totais</th>
-                        <th className="px-4 py-2">Escherichia coli</th>
-                        <th className="px-4 py-2">Cor</th>
-                        <th className="px-4 py-2">Data</th>
-                        <th className="px-4 py-2">Responsáveis</th>
-                        <th className="px-4 py-2">Status</th>
-                        <th className="px-4 py-2">Editar</th>
+                    <tr className="bg-slate-300 text-neutral-800">
+                        <th className="px-2 py-1">ID</th>
+                        <th className="px-2 py-1">Ordem</th>
+                        <th className="px-2 py-1">Temperatura</th>
+                        <th className="px-2 py-1">Cloro residual livre</th>
+                        <th className="px-2 py-1">Turbidez</th>
+                        <th className="px-2 py-1">Coliformes totais</th>
+                        <th className="px-2 py-1">Escherichia coli</th>
+                        <th className="px-2 py-1">Cor</th>
+                        <th className="px-2 py-1">Data</th>
+                        <th className="px-2 py-1">Responsáveis</th>
+                        <th className="px-2 py-1">Status</th>
+                        <th className="px-2 py-1">Editar</th>
                     </tr>
                 </thead>    
                 <tbody>
                     {coletas.map(coleta => {
                         return (
-                            <tr className="bg-sky-200 even:bg-sky-50">
-                                <td className="px-4 py-2">{coleta.id}</td>
-                                <td className="px-4 py-2">{coleta.ordem}</td>
-                                <td className="px-4 py-2">{coleta.temperatura} ºC</td>
-                                <td className="px-4 py-2">{coleta.cloro_residual_livre} mg/L</td>
-                                <td className="px-4 py-2">{coleta.turbidez} uT </td>
-                                <td className="px-4 py-2">{coleta.coliformes_totais ? 'Presença' : 'Ausência'}</td>
-                                <td className="px-4 py-2">{coleta.escherichia ? 'Presença' : 'Ausência'}</td>
-                                <td className="px-4 py-2">{coleta.cor}</td>
-                                <td className="px-4 py-2">{coleta.data}</td>
-                                <td className="px-4 py-2">responsaveis</td>
-                                <td className="px-4 py-2 text-center">
+                            <tr className="bg-slate-200 even:bg-slate-100">
+                                <td className="px-2 py-3">{coleta.id}</td>
+                                <td className="px-2 py-3">{coleta.ordem}</td>
+                                <td className="px-2 py-3">{coleta.temperatura} ºC</td>
+                                <td className="px-2 py-3">{coleta.cloro_residual_livre} mg/L</td>
+                                <td className="px-2 py-3">{coleta.turbidez} uT </td>
+                                <td className="px-2 py-3">{coleta.coliformes_totais ? 'Presença' : 'Ausência'}</td>
+                                <td className="px-2 py-3">{coleta.escherichia ? 'Presença' : 'Ausência'}</td>
+                                <td className="px-2 py-3">{coleta.cor}</td>
+                                <td className="px-2 py-3">{coleta.data}</td>
+                                <td className="px-2 py-3">responsaveis</td>
+                                <td className="px-2 py-3 text-center">
                                     {coleta.status.status ? <i className="bi bi-check2"></i> : <i className="bi bi-x"></i>}
                                     {coleta.status.message}
                                 </td>
-                                <td className="px-4 py-2">
+                                <td className="px-2 py-3">
                                     <a 
                                         href={`/coletas/${coleta.id}`} 
                                         className="text-neutral-600 font-medium hover:text-blue-600"
