690c55715210cebd15db29c262cbb2c29e8024af
Thiago Coelho <thiago.coelho158@gmail.com>
2024-05-02 11:18:10 -0300

    feat: popular select com os pontos pertencentes a sequencia atual

--- src/app/admin/sequencias_coletas/[sequencia_id]/criar/page.tsx
+++ src/app/admin/sequencias_coletas/[sequencia_id]/criar/page.tsx
@@ -1,23 +1,172 @@
-import { API_BASE_URL } from "@/utils/config";
-import { Usuario } from "@/utils/api_consumer"
-import ColetaForm from "./ColetaForm";
+'use client'
 
-export default async function Page({ params }: { params: {
+import { TIPOS_PONTOS, Usuario } from "@/utils/types"
+import { useSequencia, usePontos, useUsuarios } from "@/utils/api_consumer/client_side_consumer";
+import { FormEvent } from "react";
+
+// import ColetaForm from "./ColetaForm";
+
+export default function Page({ params }: { params: {
     sequencia_id: number
 }}) {
-    // const sequencia = useSequencia(params.sequencia_id);
-    // const pontos = usePontos(sequencia?.ponto_url);
-    const resp = await fetch(`${API_BASE_URL}/api/v1/usuarios`);
-    const users: Usuario[] = (await resp.json()).items;
+    const { sequencia_id } = params;
+    const sequencia = useSequencia(sequencia_id);
+    const pontos = usePontos(
+        sequencia ? sequencia.ponto_url : null
+    );
+
+    const usuarios = useUsuarios();
+
+    function submitForm(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        for (let i = 0; i < 100; i++) {
+            console.log("HIIIII", i);
+        }
+
+        (async () => {
+            const formData = new FormData(event.currentTarget);
+    
+            const data = {
+                sequencia_id: sequencia_id,
+                ponto_id: Number(formData.get("ponto")),
+                temperatura: Number(formData.get("temperatura")),
+                cloro_residual_livre: Number(formData.get("cloro_residual_livre")),
+                turbidez: Number(formData.get("turbidez")),
+                coliformes_totais: formData.get("coliformes_totais") == "on",
+                escherichia: formData.get("escherichia") == "on",
+                cor: Number(formData.get("cor")),
+                responsavel: [Number(formData.get("responsaveis"))],
+                data: new Date(String(formData.get("data"))),
+                ordem: formData.get("ordem"),
+            };
+    
+            const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/v1/coletas/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+    
+            window.location.href = `/admin/sequencias_coletas/${sequencia_id}`;
+        })()
+    };
 
     return (
         <>
             <h1 className="text-4xl text-neutral-700 font-bold mb-8">Criar nova coleta</h1>
-            <ColetaForm 
-                users={users}
-                api_url={API_BASE_URL}
-                sequencia_id={params.sequencia_id}
-            ></ColetaForm>
+            <form className="w-full flex flex-col gap-4" onSubmit={submitForm}>
+                <label htmlFor="ponto">Ponto de Coleta:</label>                        
+                <select
+                    name="ponto"
+                    className="rounded-lg border border-neutral-400 px-6 py-4"
+                >
+                    {/* <!-- Os pontos serão adicionados aqui --> */}
+                    <option>-</option>
+                    {pontos.map(ponto => <option value={ponto.id} key={"ponto " + ponto.id}>{ponto.id} - {TIPOS_PONTOS[ponto.tipo]}, {ponto.ambiente}</option>)}
+                </select>
+
+                <label htmlFor="temperatura">Temperatura:</label>
+                <input
+                    type="number"
+                    id="temperatura"
+                    name="temperatura"
+                    className="rounded-lg border border-neutral-400 px-6 py-4"
+                    required
+                />
+
+                <label htmlFor="cloro_residual_livre">Cloro Residual Livre:</label>
+                <input
+                    type="number"
+                    id="cloro_residual_livre"
+                    name="cloro_residual_livre"
+                    className="rounded-lg border border-neutral-400 px-6 py-4"
+                    required
+                />
+
+                <label htmlFor="turbidez">Turbidez:</label>
+                <input
+                    type="number"
+                    id="turbidez"
+                    name="turbidez"
+                    className="rounded-lg border border-neutral-400 px-6 py-4"
+                    required
+                />
+
+                <div className="flex gap-2">
+                    <input
+                        type="checkbox"
+                        id="coliformes_totais"
+                        name="coliformes_totais"
+                        className="w-6 h-6 rounded-lg border border-neutral-400 px-6 py-4"
+                    />
+                    <label htmlFor="coliformes">Coliformes Totais</label>
+                </div>
+                
+                <div className="flex gap-2">
+                    <input
+                        type="checkbox"
+                        id="escherichia"
+                        name="escherichia"
+                        className="w-6 h-6 rounded-lg border border-neutral-400 px-6 py-4"
+                    />
+                    <label htmlFor="escherichia">Escherichia</label>
+                </div>
+
+                <label htmlFor="cor">Cor:</label>
+                <input
+                    type="number"
+                    id="cor"
+                    name="cor"
+                    className="rounded-lg border border-neutral-400 px-6 py-4"
+                    required
+                />
+
+                <div className="flex gap-4">
+                    <label htmlFor="data">Data e hora:</label>
+                    <input
+                        type="date"
+                        id="data"
+                        name="data"
+                        className="rounded-lg border border-neutral-400 px-6 py-4 flex-grow"
+                        required
+                    />
+
+                    <input
+                        type="time"
+                        id="hora"
+                        name="hora"
+                        className="rounded-lg border border-neutral-400 px-6 py-4"
+                        required
+                    />
+                </div>
+
+                <label htmlFor="responsaveis">Responsáveis:</label>
+                <select 
+                    name="responsaveis"
+                    id="responsaveis"
+                    className="rounded-lg border border-neutral-400 px-6 py-4"
+                    multiple
+                >
+                    {usuarios.map(usuario => {
+                        return <option value={usuario.id} key={"usuario" + usuario.id}>{usuario.username}</option>
+                    })}
+                </select>
+
+                <label htmlFor="ordem">Ordem:</label>
+                <select
+                    name="ordem"
+                    id="ordem"
+                    className="rounded-lg border border-neutral-400 px-6 py-4"
+                >
+                    <option value="Coleta">Coleta</option>
+                    <option value="Recoleta">Recoleta</option>
+                </select>
+
+                <div className="rounded-lg border border-neutral-400 px-6 py-4 bg-primary-500 hover:bg-primary-600 text-white font-semibold text-center">
+                    <input id="criar" type="submit" value="Criar" />
+                </div>
+            </form>
         </>
     )
 }
