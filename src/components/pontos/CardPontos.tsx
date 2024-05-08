0760bb86dd4084fb185090f7ef9aa4c60371c566
Thiago Coelho <thiago.coelho158@gmail.com>
2024-05-08 00:47:37 -0300

    feat: remover nome da edificação de card de ponto

--- src/components/pontos/CardPontos.tsx
+++ src/components/pontos/CardPontos.tsx
@@ -1,4 +1,4 @@
-import { Ponto } from "@/utils/api_consumer";
+import { Ponto, TIPOS_PONTOS} from "@/utils/types";
 
 
 export default function CardPonto(props: { ponto: Ponto }) {
@@ -7,10 +7,11 @@ export default function CardPonto(props: { ponto: Ponto }) {
 
   return (
     ponto && (
-      <div className="relative flex aspect-square w-[260px] flex-col items-center justify-between rounded-md border border-neutral-300 bg-white px-2 py-4 text-center shadow-lg">
+      <div className="relative flex aspect-square w-[260px] flex-col items-center justify-between rounded-md border border-neutral-300 bg-white px-2 py-4 text-center">
         <h3>{edificacao?.codigo}</h3>
-        <h2 className="text-center text-xl">{edificacao?.nome}</h2>
-        <p className="text-sm">{ponto.tipo === 1 ? "Bebedouro" : "RPS"}</p>
+        {/* <h2 className="text-center text-xl">{edificacao?.nome}</h2> */}
+        <h2 className="text-center text-xl">{TIPOS_PONTOS[ponto.tipo - 1]}</h2>
+        {/* <p className="text-sm">{ponto.tipo === 1 ? "Bebedouro" : "RPS"}</p> */}
         <p className="text-sm">{ponto.ambiente}</p>
 
         <a
