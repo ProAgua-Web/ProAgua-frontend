44da50cb2a43335b16194fe27b52f971291ca697
Thiago Coelho <thiago.coelho158@gmail.com>
2024-05-07 22:56:07 -0300

    feat: atualizar stylesheet de página pública de ponto

--- src/app/(public)/pontos/[id_ponto]/page.tsx
+++ src/app/(public)/pontos/[id_ponto]/page.tsx
@@ -1,42 +1,40 @@
 import Header from "@/components/layout/Header";
 import { Coleta, Ponto } from "@/utils/types";
-import { API_BASE_URL } from "@/utils/config";
-
 
 function PointDashboard(props: {coleta: Coleta}) {
     const { coleta } = props;
     const bg_color = coleta.status ? "bg-green-200" : "bg-orange-200";
     return (
-        <div className="flex gap-8 p-8">
-            <div className={"rounded-lg p-4 shadow " + bg_color}>
+        <div className="flex gap-4 flex-wrap">
+            <div className={"w-2/5 aspect-square rounded-lg p-6 shadow " + bg_color}>
                 <h1>Coliformes totais</h1>
                 <p>
                     {coleta.coliformes_totais ? "Presença" : "Ausência"}
                 </p>
             </div>
             
-            <div className={"rounded-lg p-4 shadow " + bg_color}>
+            <div className={"w-2/5 aspect-square rounded-lg p-6 shadow " + bg_color}>
                 <h1>Escherichia coli</h1>
                 <p>
                     {coleta.escherichia ? "Presença" : "Ausência"}
                 </p>
             </div>
             
-            <div className={"rounded-lg p-4 shadow " + bg_color}>
+            <div className={"w-2/5 aspect-square rounded-lg p-6 shadow " + bg_color}>
                 <h1>Turbidez</h1>
                 <p>
                     {coleta.turbidez} uT
                 </p>
             </div>
             
-            <div className={"rounded-lg p-4 shadow " + bg_color}>
+            <div className={"w-2/5 aspect-square rounded-lg p-6 shadow " + bg_color}>
                 <h1>Cloro residual livre</h1>
                 <p>
                     {coleta.cloro_residual_livre} mg/L
                 </p>
             </div>
 
-            <div className={"rounded-lg p-4 shadow " + bg_color}>
+            <div className={"w-2/5 aspect-square rounded-lg p-6 shadow " + bg_color}>
                 <h1>Temperatura</h1>
                 <p>
                     {coleta.temperatura} ºC
@@ -50,10 +48,10 @@ export default async function Page(props: {
     params: { id_ponto:number }
 }) {
     const { params } = props;
-    var resp = await fetch(`${API_BASE_URL}/api/v1/pontos/${params.id_ponto}`);
+    var resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/pontos/${params.id_ponto}`);
     var ponto: Ponto = await resp.json();
     
-    resp = await fetch(`${API_BASE_URL}/api/v1/pontos/${params.id_ponto}/coletas`);
+    resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/pontos/${params.id_ponto}/coletas`);
     var coletas: Coleta[] = await resp.json();
     var coleta = coletas[coletas.length - 1];
 
@@ -68,7 +66,7 @@ export default async function Page(props: {
                     Ambiente: {ponto.ambiente}<br />
                     Status: {ponto.status ? "Em conformidade" : "Não conforme"}<br />
                 </div>
-                <div>
+                <div className="mt-4">
                     {/* {coleta && 
                         <>
                             id: {coleta.id}; 
