ae904e15651fdb221aec5d0334de564255ab0fd4
vynijales <vynijales@gmail.com>
2024-05-09 04:37:05 -0300

    feat: acessar a criação de edificações e pontos de coleta em uma única página

---
+++ src/app/admin/pontos/criar/[cod_edificacao]/page.tsx
@@ -0,0 +1,143 @@
+"use client"
+
+import { Edificacao, Ponto, TIPOS_PONTOS } from "@/utils/types";
+import { useEffect, useState } from "react";
+
+export default function Pontos({ params }: { params: { cod_edificacao: string } }) {
+    // TODO: adicionar campo para adição de imagem
+
+    const [edificacoes, setEdificacoes] = useState<Edificacao[]>([]);
+    const [pontos, setPontos] = useState<Ponto[]>([]);
+
+    const submitForm = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+
+        const target = e.target as typeof e.target & {
+            edificacao: { value: string };
+            ambiente: { value: string };
+            tombo: { value: string };
+            tipo: { value: string };
+            amontante: { value: string };
+        };
+
+        const data = {
+            codigo_edificacao: target.edificacao.value,
+            ambiente: target.ambiente.value,
+            tombo: target.tombo.value,
+            tipo: parseInt(target.tipo.value),
+            amontante: parseInt(target.amontante.value),
+        };
+
+        fetch(process.env.NEXT_PUBLIC_API_URL + "/api/v1/pontos/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Erro ao criar ponto");
+                }
+            })
+            .then(() => {
+                window.location.href = "/admin/pontos";
+            })
+            .catch((err) => {
+                alert(err);
+            });
+    };
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/v1/edificacoes");
+            setEdificacoes((await response.json()).items);
+        })();
+    }, []);
+
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/v1/pontos");
+            setPontos((await response.json()).items);
+        })();
+    }, []);
+
+    return (
+        <>
+            <h1 className="mb-4 text-3xl font-bold text-neutral-600">Criar ponto</h1>
+
+            <form
+                method="post"
+                className="flex flex-col rounded-xl border border-neutral-200 p-8 shadow-lg"
+                onSubmit={submitForm}
+            >
+
+                {edificacoes.length > 0 && (
+                    <>
+                        <label htmlFor="" className="mt-4">Edificação:</label>
+                        <select
+                            id="edificacao"
+                            name="edificacao"
+                            className="bg-white rounded-md border border-neutral-200 px-4 py-2"
+                            defaultValue={params.cod_edificacao}
+                        >
+                            {edificacoes.map((edificacao: Edificacao) => {
+                                return <option value={edificacao.codigo} >{edificacao.codigo} - {edificacao.nome}</option>
+                            })}
+                        </select >
+                    </>
+                )
+                }
+                <label htmlFor="" className="mt-4">Ambiente:</label>
+                <input
+                    type="text"
+                    id="ambiente"
+                    name="ambiente"
+                    className="bg-white rounded-md border border-neutral-200 px-4 py-2"
+                />
+
+                <label htmlFor="" className="mt-4">Tombo:</label>
+                <input
+                    type="text"
+                    id="tombo"
+                    name="tombo"
+                    className="bg-white rounded-md border border-neutral-200 px-4 py-2"
+                />
+
+                <label htmlFor="" className="mt-4">Tipo:</label>
+                <select
+                    id="tipo"
+                    name="tipo"
+                    className="bg-white rounded-md border border-neutral-200 px-4 py-2"
+                >
+                    <option value="1">Bebedouro</option>
+                    <option value="2">RPS - Reservatório predial superior</option>
+                    <option value="3">RPI - Reservatório predial inferior</option>
+                    <option value="4">RDS - Reservatório de destribuição superior</option>
+                    <option value="5">RDI - Reservatório de destribuição inferior</option>
+                </select>
+
+                <label htmlFor="" className="mt-4">Ponto a montante:</label>
+                <select
+                    id="amontante"
+                    name="amontante"
+                    className="bg-white rounded-md border border-neutral-200 px-4 py-2"
+                >
+                    <option value="">-</option>
+                    {pontos.map((ponto: Ponto) => {
+                        console.log(ponto)
+                        return <option className="" value={ponto.id}>{TIPOS_PONTOS[ponto.tipo - 1]} {ponto.ambiente.trim() != "-" && ponto.ambiente.trim() != "nan" && ponto.ambiente.trim() != "" ? "- " + ponto.ambiente : ""} {ponto.tombo.trim() != "-" && ponto.tombo.trim() != "nan" && ponto.tombo.trim() ? "- " + ponto.tombo : ""}</option>
+                    })}
+                </select>
+
+                <input
+                    type="submit"
+                    value="Criar"
+                    className="mt-8 w-full self-end rounded-md bg-green-500 px-4 py-2 font-semibold text-white"
+                />
+            </form>
+        </>
+    );
+}
+
--- src/app/admin/pontos/page.tsx
+++ src/app/admin/pontos/page.tsx
@@ -1,40 +1,44 @@
 import Filters from "@/components/sequencias/Filters";
-import CardPonto from "@/components/pontos/CardPontos";
+import CardPonto, { AddCard } from "@/components/pontos/CardPontos";
 import { Ponto } from "@/utils/types";
 
 function groupBy<Type>(arr: Type[], key: (el: Type) => string) {
   var groups = Object();
-  
+
   arr.forEach(element => {
-      let groupName = key(element);
-      let group: Object[] = groups[groupName] || [];
-      group.push(element);
-      groups[groupName] = group;
+    let groupName = key(element);
+    let group: Object[] = groups[groupName] || [];
+    group.push(element);
+    groups[groupName] = group;
   })
 
   return groups;
 }
 
-function CardEdificacao(props: {group: [string, Ponto[]]}) {
+function CardEdificacao(props: { group: [string, Ponto[]] }) {
   const { group } = props;
 
   return (
-    <div className="mb-8 rounded bg-gradient-to-b from-gray-100 to-gray-50 border border-gray-300">
-      <div className="flex justify-between p-4 py-2">
-        <h3 className="text-xl font-semibold text-black">{group[0]}</h3>
-        {/* <a href="/admin/pontos/criar" className="p-4 mb-4 w-48 bg-green-500 text-white font-semibold rounded-md hover:bg-green-600">+ Adicionar ponto</a> */}
-      </div>
-      <div className="p-4 flex gap-4 flex-wrap">
-        {group[1].map((item, i) => (
-          <CardPonto ponto={item} key={"ponto-" + i} />
-        ))}
-      </div>
-    </div>
+        <div className="mb-8 rounded bg-gradient-to-b from-gray-100 to-gray-50 border border-gray-300">
+          <div className="flex justify-between p-4 py-2">
+              <h3 className="text-xl font-semibold text-black">{group[0]}</h3>
+              <a href={`/admin/edificacoes/${group[1][0].edificacao.codigo}`} className="hover:text-primary-600">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+    </svg></a>
+          </div>
+          <div className="p-4 flex gap-4 flex-wrap">
+            {group[1].map((item, i) => (
+              <CardPonto ponto={item} key={"ponto-" + i} />
+            ))}
+            <AddCard cod_edificacao={group[1][0].edificacao.codigo}/>
+          </div>
+        </div>
   )
 }
 
 export default async function Pontos() {
-  const resp = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/v1/pontos?limit=10000', {cache: "no-cache"});
+  const resp = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/v1/pontos?limit=10000', { cache: "no-cache" });
   const pontos: Ponto[] = (await resp.json()).items;
   const groups = groupBy<Ponto>(pontos, (ponto: Ponto) => {
     return `${ponto.edificacao.codigo} - ${ponto.edificacao.nome}`
@@ -45,7 +49,7 @@ export default async function Pontos() {
       <h2 className="text-3xl text-[#525252]">Pontos de Coleta</h2>
       <div className="flex w-full flex-col items-center">
         <Filters />
-        <a href="/admin/pontos/criar" className="p-2 px-4 mb-4 w-full bg-gray-100 border border-gray-300 text-green-500 font-semibold rounded-md hover:bg-green-600 hover:text-white text-center">+ Adicionar ponto</a>
+        <a href="/admin/edificacoes/criar" className="p-2 px-4 mb-4 w-full bg-gray-100 border border-gray-300 text-green-500 font-semibold rounded-md hover:bg-green-600 hover:text-white text-center">+ Adicionar edificação</a>
         <div className="flex flex-col w-full">
           {Object.entries(groups).map((group) => {
             return (
