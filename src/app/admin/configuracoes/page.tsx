8f863961592e5225783e9163c94cdb3584e0b4f6
vynijales <vynijales@gmail.com>
2024-05-09 23:35:11 -0300

    feat: habilitar criação de usuário pela interface

--- src/app/admin/configuracoes/page.tsx
+++ src/app/admin/configuracoes/page.tsx
@@ -6,7 +6,6 @@ export default async function Configuracoes() {
 
     return (
         <>
-            
             <h1 className="text-3xl font-semibold mb-2">Configurações</h1>
 
             <h2 className="text-2xl font-semibold mt-8 text-neutral-700">Usuários</h2>
@@ -19,7 +18,11 @@ export default async function Configuracoes() {
                         </li>
                     ))}
                 </ul>
-                <button className="ml-auto w-fit px-4 py-2 bg-green-500 border border-green-600 rounded text-white font-semibold">+ Adicionar usuário</button>
+
+                <div className="w-full flex justify-center">
+                    <a href="/admin/configuracoes/usuarios/criar" className=" w-fit px-4 py-2 bg-green-500 border border-green-600 rounded text-white font-semibold">+ Adicionar usuário</a>
+                </div>
+
             </div>
 
 
@@ -28,32 +31,32 @@ export default async function Configuracoes() {
             <div className="w-full bg-white shadow-lg rounded border border-neutral-300 p-4">
                 <div className="w-full flex gap-4 p-4 border-b border-neutral-300">
                     <label className="flex-grow">Temperatura</label>
-                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor mínimo"/>
-                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor máximo"/>
+                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor mínimo" />
+                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor máximo" />
                 </div>
 
                 <div className="w-full flex gap-4 p-4 border-b border-neutral-300">
                     <label className="flex-grow">Cloro residual livre</label>
-                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor mínimo"/>
-                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor máximo"/>
+                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor mínimo" />
+                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor máximo" />
                 </div>
 
                 <div className="w-full flex gap-4 p-4 border-b border-neutral-300">
                     <label className="flex-grow">Turbidez</label>
-                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor mínimo"/>
-                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor máximo"/>
+                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor mínimo" />
+                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor máximo" />
                 </div>
 
                 <div className="w-full flex gap-4 p-4 border-b border-neutral-300">
                     <label className="flex-grow">Cor</label>
-                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor mínimo"/>
-                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor máximo"/>
+                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor mínimo" />
+                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor máximo" />
                 </div>
-                
+
                 <div className="w-full flex gap-4 p-4">
                     <label className="flex-grow">Temperatura</label>
-                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor mínimo"/>
-                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor máximo"/>
+                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor mínimo" />
+                    <input className="w-48 px-4 py-2 border border-neutral-300 rounded" type="number" step="0.1" placeholder="valor máximo" />
                 </div>
                 <button className="px-4 py-2 rounded bg-rose-500 text-white font-semibold hover:bg-rose-600 border border-rose-700">
                     Habilitar edição
