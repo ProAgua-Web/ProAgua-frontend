9336c2d8591807108c85114b280516def44188ca
vynijales <vynijales@gmail.com>
2024-05-11 01:30:44 -0300

    feat: criar visualizador de edificação nas páginas de criação/edição

--- src/app/admin/pontos/criar/[cod_edificacao]/page.tsx
+++ src/app/admin/pontos/criar/[cod_edificacao]/page.tsx
@@ -11,10 +11,16 @@ export default function Pontos({ params }: { params: { cod_edificacao: string } 
     const [preview, setPreview] = useState<string>();
     const [submiting, setSubmiting] = useState<boolean>(false);
     let [currentAmontante, setCurrentAmontante] = useState<string>('');
+    let [currentEdificacao, setCurrentEdificacao] = useState<string>('');
 
     function updateAmontante() {
         const amontante = document.getElementById("amontante") as HTMLSelectElement;
         setCurrentAmontante(amontante.value);
+    }
+
+    function updateEdificacao() {
+        const edificacao = document.getElementById("edificacao") as HTMLSelectElement;
+        setCurrentEdificacao(edificacao.value);
     }
 
     const selectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -115,22 +121,38 @@ export default function Pontos({ params }: { params: { cod_edificacao: string } 
             <h1 className="text-4xl text-neutral-700 font-bold mb-8">Criar ponto de coleta</h1>
 
             <form className="w-full flex flex-col gap-4" onSubmit={(e) => submitForm(e)} method="POST">
-
-                <label htmlFor="">Edificação:</label>
-
+                
                 {
                     edificacoes.length > 0 && (
-                        <select
-                            id="edificacao"
-                            name="edificacao"
-                            className="rounded-lg border border-neutral-400 px-6 py-4"
-                            defaultValue={params.cod_edificacao}
-                        >
-                            <option value="">-</option>
-                            {edificacoes.map((edificacao: Edificacao) => {
-                                return <option value={edificacao.codigo} >{edificacao.codigo} - {edificacao.nome}</option>
-                            })}
-                        </select >
+                        <>
+                            <label htmlFor="">Edificação:</label>
+
+
+                            <div className="flex">
+                                <select
+                                    id="edificacao"
+                                    name="edificacao"
+                                    className="w-full rounded-lg border border-neutral-400 px-6 py-4"
+                                    onChange={updateEdificacao}
+                                >
+                                    <option value="">-</option>
+                                    {edificacoes.map((edificacao: Edificacao) => {
+                                        return <option value={edificacao.codigo} >{edificacao.codigo} - {edificacao.nome}</option>
+                                    })}
+                                </select >
+
+                                <a className="flex justify-center" href={currentEdificacao ? "/admin/edificacoes/" + currentEdificacao : "#"} target={currentEdificacao ? "_blank" : "_self"}>
+                                    <svg className={currentEdificacao ? `w-6 mx-4 fill-primary-600` : `w-6 mx-4 fill-neutral-500`}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 576 512">
+                                        <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.
+                        2 3.3 20.3z" />
+                                    </svg>
+                                </a>
+
+                            </div>
+
+                        </>
                     )
 
                 }
--- src/app/admin/pontos/criar/page.tsx
+++ src/app/admin/pontos/criar/page.tsx
@@ -12,6 +12,7 @@ export default function Pontos() {
 
     const [submiting, setSubmiting] = useState<boolean>(false);
     let [currentAmontante, setCurrentAmontante] = useState<string>('');
+    let [currentEdificacao, setCurrentEdificacao] = useState<string>('');
 
     const selectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
         setFile(event.target.files?.[0]);
@@ -21,6 +22,11 @@ export default function Pontos() {
     function updateAmontante() {
         const amontante = document.getElementById("amontante") as HTMLSelectElement;
         setCurrentAmontante(amontante.value);
+    }
+
+    function updateEdificacao() {
+        const edificacao = document.getElementById("edificacao") as HTMLSelectElement;
+        setCurrentEdificacao(edificacao.value);
     }
 
     async function submitForm(event: FormEvent<HTMLFormElement>) {
@@ -118,16 +124,29 @@ export default function Pontos() {
             <form className="w-full flex flex-col gap-4" onSubmit={(e) => submitForm(e)} method="POST">
 
                 <label htmlFor="">Edificação:</label>
-                <select
-                    id="edificacao"
-                    name="edificacao"
-                    className="rounded-lg border border-neutral-400 px-6 py-4"
-                >
-                    <option value="">-</option>
-                    {edificacoes.map((edificacao: Edificacao) => {
-                        return <option value={edificacao.codigo} >{edificacao.codigo} - {edificacao.nome}</option>
-                    })}
-                </select >
+                <div className="flex">
+                    <select
+                        id="edificacao"
+                        name="edificacao"
+                        className="w-full rounded-lg border border-neutral-400 px-6 py-4"
+                        onChange={updateEdificacao}
+                    >
+                        <option value="">-</option>
+                        {edificacoes.map((edificacao: Edificacao) => {
+                            return <option value={edificacao.codigo} >{edificacao.codigo} - {edificacao.nome}</option>
+                        })}
+                    </select >
+
+                    <a className="flex justify-center" href={currentEdificacao ? "/admin/edificacoes/" + currentEdificacao : "#"} target={currentEdificacao ? "_blank" : "_self"}>
+                        <svg className={currentEdificacao ? `w-6 mx-4 fill-primary-600` : `w-6 mx-4 fill-neutral-500`}
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 576 512">
+                            <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.
+                        2 3.3 20.3z" />
+                        </svg>
+                    </a>
+
+                </div>
 
                 <label htmlFor="">Ambiente:</label>
                 <input
