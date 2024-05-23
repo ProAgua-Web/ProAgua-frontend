e2860c739e5cb359b4b63c24fd15e30a57ee9b82
vynijales <vynijales@gmail.com>
2024-05-23 18:49:21 -0300

    style: melhorar ux design

--- src/app/(public)/pontos/[id_ponto]/page.tsx
+++ src/app/(public)/pontos/[id_ponto]/page.tsx
@@ -4,11 +4,17 @@ import Header from "@/components/layout/Header";
 import { useLastColetaByPonto, usePonto, useParametrosReferencia } from "@/utils/api_consumer/client_side_consumer";
 import { Coleta } from "@/utils/types";
 import { use } from "react";
+import Image from "next/image";
 
-function PointDashboard(props: {coleta: Coleta}) {
+import Conformidade from "/public/conformidade.svg";
+import NaoConformidade from "/public/nao_conformidade.svg";
+import Alert from "/public/alert.svg";
+
+function PointDashboard(props: { coleta: Coleta }) {
     const { coleta } = props;
     const bg_color_ok = "bg-green-200";
     const bg_color_not_ok = "bg-red-200";
+    const bg_color_alert = "bg-yellow-200";
 
     const referencia = useParametrosReferencia();
 
@@ -44,49 +50,84 @@ function PointDashboard(props: {coleta: Coleta}) {
 
     return (
         <div className="flex gap-4 flex-wrap">
-            <div className={`w-full rounded-lg p-6 shadow ${coleta.coliformes_totais ? bg_color_not_ok : bg_color_ok}`}>
-                <h1>Coliformes totais</h1>
-                <p>
-                    {coleta.coliformes_totais ? "Presença" : "Ausência"}
-                </p>
-            </div>
-            
-            <div className={`w-full rounded-lg p-6 shadow ${coleta.escherichia ? bg_color_not_ok : bg_color_ok}`}>
-                <h1>Escherichia coli</h1>
-                <p>
-                    {coleta.escherichia ? "Presença" : "Ausência"}
-                </p>
-            </div>
-            
-            <div className={`w-full rounded-lg p-6 shadow ${turbidezInterval() ? bg_color_ok : bg_color_not_ok}`}>
-                <h1>Turbidez</h1>
-                <p>
-                    {coleta.turbidez} uT
-                </p>
-            </div>
-            
-            <div className={`w-full rounded-lg p-6 shadow ${cloroInterval() ? bg_color_ok : bg_color_not_ok}`}>
-                <h1>Cloro residual livre</h1>
-                <p>
-                    {coleta.cloro_residual_livre} mg/L
-                </p>
+            <div className={`w-full flex justify-between items-center rounded-lg p-6 shadow ${coleta.coliformes_totais ? bg_color_not_ok : bg_color_ok}`}>
+                <div>
+                    <h1 className="text-lg font-bold">Coliformes totais</h1>
+                    <p className="p-2 text-md">
+                        {coleta.coliformes_totais ? "PRESENÇA" : "AUSÊNCIA"} em 100 mL <br />
+                        {coleta.coliformes_totais ? "Em conformidade" : "Não conformidade"}
+                    </p>
+                    <span className="text-sm text-gray-700">VMP<sup>(1)</sup>: {coleta.coliformes_totais ? "Presença" : "Ausência"} em 100 mL</span>
+                </div>
+                <Image src={coleta.coliformes_totais ? NaoConformidade : Conformidade}
+                    alt="Coliformes totais"
+                    className="h-24 w-24" />
             </div>
 
-            <div className={`w-full rounded-lg p-6 shadow ${temperaturaInterval() ? bg_color_ok : bg_color_not_ok}`}>
-                <h1>Temperatura</h1>
-                <p>
-                    {coleta.temperatura} ºC
-                </p>
+            <div className={`w-full flex justify-between items-center rounded-lg p-6 shadow ${coleta.escherichia ? bg_color_not_ok : bg_color_ok}`}>
+                <div>
+                    <h1 className="text-lg font-bold">Escherichia coli</h1>
+                    <p className="p-2 text-md">
+                        {coleta.escherichia ? "PRESENÇA" : "AUSÊNCIA"} em 100 mL <br />
+                        {coleta.escherichia ? "Em conformidade" : "Não conformidade"}
+                    </p>
+                    <span className="text-sm text-gray-700">VMP<sup>(1)</sup>: {coleta.escherichia ? "Presença" : "Ausência"} em 100 mL</span>
+                </div>
+                <Image src={coleta.escherichia ? NaoConformidade : Conformidade}
+                    alt="Escherichia coli"
+                    className="h-24 w-24" />
+            </div>
+
+            <div className={`w-full flex justify-between items-center rounded-lg p-6 shadow ${turbidezInterval() ? bg_color_ok : bg_color_not_ok}`}>
+                <div>
+                    <h1 className="text-lg font-bold">Turbidez</h1>
+                    <p className="p-2 text-md">
+                        {coleta.turbidez} uT <br />
+                        {turbidezInterval() ? "Em conformidade" : "Não conformidade"}
+                    </p>
+                    <span className="text-sm text-gray-700">VMP<sup>(1)</sup>: {referencia?.max_cloro_residual_livre} uT</span>
+                </div>
+                <Image src={turbidezInterval() ? Conformidade : NaoConformidade}
+                    alt="Turbidez"
+                    className="h-24 w-24" />
+            </div>
+
+            <div className={`w-full flex justify-between items-center rounded-lg p-6 shadow ${cloroInterval() ? bg_color_ok : bg_color_not_ok}`}>
+                <div>
+                    <h1 className="text-lg font-bold">Cloro residual livre</h1>
+                    <p className="p-2 text-md">
+                        {coleta.cloro_residual_livre} mg/L <br />
+                        {cloroInterval() ? "Em conformidade" : "Não conformidade"}
+                    </p>
+                    <span className="text-sm text-gray-700">Entre<sup>(1)</sup> {referencia?.min_cloro_residual_livre} e {referencia?.max_cloro_residual_livre} mg/L</span>
+                </div>
+                <Image src={cloroInterval() ? Conformidade : NaoConformidade}
+                    alt="Cloro residual livre"
+                    className="h-24 w-24" />
+            </div>
+
+            <div className={`w-full flex justify-between items-center rounded-lg p-6 shadow ${temperaturaInterval() ? bg_color_ok : bg_color_alert}`}>
+                <div>
+                    <h1 className="text-lg font-bold">Temperatura</h1>
+                    <p className="p-2 text-md">
+                        {coleta.temperatura} ºC <br />
+                        {temperaturaInterval() ? "Em conformidade" : "Alerta"}
+                    </p>
+                    <span className="text-sm text-gray-700">Ref. assumida<sup>(2)</sup> {referencia?.min_temperatura} a {referencia?.max_temperatura} °C</span>
+                </div>
+                <Image src={temperaturaInterval() ? Conformidade : Alert}
+                    alt="Temperatura"
+                    className="h-24 w-24" />
             </div>
         </div>
     )
 }
 
-export default function Page(props: { 
-    params: { id_ponto:number }
+export default function Page(props: {
+    params: { id_ponto: number }
 }) {
     const { params } = props;
-    const ponto = usePonto(params.id_ponto);    
+    const ponto = usePonto(params.id_ponto);
     var coleta = useLastColetaByPonto(params.id_ponto);
 
     return (
@@ -94,17 +135,19 @@ export default function Page(props: {
             <Header />
             <div className="relative m-auto flex w-[clamp(320px,90vw-2rem,1200px)] flex-col items-center gap-4 p-8 pt-[calc(clamp(50px,8vh,100px)+2rem)]">
                 <div className="w-full p-4 rounded border border-neutral-300 shadow">
-                    ID ponto: {ponto?.id}<br />
-                    Prédio: {ponto?.edificacao.codigo} - {ponto?.edificacao.nome} {ponto?.edificacao.campus}
-                    <br />
-                    Ambiente: {ponto?.ambiente}<br />
-                    Status: {ponto?.status ? "Em conformidade" : "Não conforme"}<br />
-                </div>
-                <div className="mt-4">
-                    {coleta && <PointDashboard coleta={coleta} />}
+                    {/* ID ponto: {ponto?.id}<br /> */}
+                    <span className="text-md text-gray-700">UFERSA Campus Mossoró</span>
+                    <h1 className="text-2xl font-semibold">{ponto?.edificacao.nome}</h1>
+                    <p className="text-lg text-black">
+                        Ponto de Coleta - {ponto?.ambiente}<br />
+                        {coleta && coleta.data ? `Última coleta em ${new Date(coleta.data).toLocaleDateString()}` : "Sem coleta registrada"}
+                    </p>
+                    <div className="mt-8">
+                        {coleta && <PointDashboard coleta={coleta} />}
+                    </div>
                 </div>
             </div>
-            
+
         </>
     )
 }
