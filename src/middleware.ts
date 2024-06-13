5c3dce69fd974ca14a7780dd10a083e2ee50cd26
Thiago Coelho <thiago.coelho158@gmail.com>
2024-06-13 11:50:04 -0300

    feat: implementar verificacao de token de acesso no backend

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,29 @@
-import type { NextRequest } from 'next/server'
+import { type NextRequest } from 'next/server'
  
-export function middleware(request: NextRequest) {
-    const isAuthenticated = request.cookies.get('access_token')?.value
-    console.log("Middleware", request.nextUrl.pathname);
+export async function middleware(request: NextRequest) {
+    const accessToken = request.cookies.get('access_token')?.value;
+    const csrfToken = request.cookies.get('csrftoken')?.value;
+    let isValid = false;
 
-    if (!isAuthenticated) {
+    // Verify the access token in the backend
+    if (accessToken && csrfToken) {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/verify_token`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken,
+                    'cookie': `csrftoken=${csrfToken}`
+                },
+                body: JSON.stringify({
+                    "access_token": accessToken
+                })
+            }
+        );
+        isValid = (response.status === 200);
+    }
+
+    // Check if there is an valid access token
+    if (!isValid) {
         if (request.nextUrl.pathname.startsWith("/admin")) {
             return Response.redirect(new URL('/login', request.url));
         }
