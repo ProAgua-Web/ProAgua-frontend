78aee5fdffafc2babe7f97824a43eeba5720ddcf
Thiago Coelho <thiago.coelho158@gmail.com>
2024-07-23 12:29:42 -0300

    refactor: extrair componentes para icones Colapse e Expand

--- src/app/admin/pontos/page.tsx
+++ src/app/admin/pontos/page.tsx
@@ -69,6 +69,31 @@ interface Groups {
   [x: string]: {edificacao: Edificacao, pontos: Ponto[]}
 }
 
+function CollapseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="Outline"
+      viewBox="0 0 24 24"
+      width="20"
+      height="20"
+    >
+      <path d="M1,6H23a1,1,0,0,0,0-2H1A1,1,0,0,0,1,6Z" />
+      <path d="M23,9H1a1,1,0,0,0,0,2H23a1,1,0,0,0,0-2Z" />
+      <path d="M23,19H1a1,1,0,0,0,0,2H23a1,1,0,0,0,0-2Z" />
+      <path d="M23,14H1a1,1,0,0,0,0,2H23a1,1,0,0,0,0-2Z" />
+    </svg>
+  )
+}
+
+function ExpandIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="20" height="20">
+      <path d="M19,1H5C2.24,1,0,3.24,0,6v12c0,2.76,2.24,5,5,5h14c2.76,0,5-2.24,5-5V6c0-2.76-2.24-5-5-5ZM5,3h14c1.65,0,3,1.35,3,3v2H2v-2c0-1.65,1.35-3,3-3Zm14,18H5c-1.65,0-3-1.35-3-3V10H22v8c0,1.65-1.35,3-3,3Z" />
+    </svg>
+  )
+}
+
 export default function Pontos() {
   const [filters, setFilters] = useState<any>({ q: "", campus: "" })
   const [edificacoes, setEdificacoes] = useState<Edificacao[]>([]);
@@ -255,12 +280,7 @@ export default function Pontos() {
             <button
               onClick={() => setCollapsed(!collapsed)}
               className={`self-end px-4 py-2 mb-2 border rounded-md hover:bg-slate-50`}>
-
-              {collapsed ?
-                <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="20" height="20"><path d="M1,6H23a1,1,0,0,0,0-2H1A1,1,0,0,0,1,6Z" /><path d="M23,9H1a1,1,0,0,0,0,2H23a1,1,0,0,0,0-2Z" /><path d="M23,19H1a1,1,0,0,0,0,2H23a1,1,0,0,0,0-2Z" /><path d="M23,14H1a1,1,0,0,0,0,2H23a1,1,0,0,0,0-2Z" /></svg>
-                :
-                <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="20" height="20"><path d="M19,1H5C2.24,1,0,3.24,0,6v12c0,2.76,2.24,5,5,5h14c2.76,0,5-2.24,5-5V6c0-2.76-2.24-5-5-5ZM5,3h14c1.65,0,3,1.35,3,3v2H2v-2c0-1.65,1.35-3,3-3Zm14,18H5c-1.65,0-3-1.35-3-3V10H22v8c0,1.65-1.35,3-3,3Z" /></svg>
-              }
+              { collapsed ? <CollapseIcon /> : <ExpandIcon /> }
             </button>
           </div>
         </div>
