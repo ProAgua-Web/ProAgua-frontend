a0db1f9bf8fcf17bd051f0f1097ec02350ec5f0b
vynijales <vynijales@gmail.com>
2024-08-02 23:17:42 -0300

    feat: criar componentes

---
+++ src/components/widgets/spinner.tsx
@@ -0,0 +1,11 @@
+const Spinner = () => {
+    
+    return (
+        <span className="w-6 h-6 block border-4 border-l-white border-blue-400 rounded-full animate-spin mr-4"/>
+    )
+
+}
+
+Spinner.displayName = "Spinner";
+
+export default Spinner;
