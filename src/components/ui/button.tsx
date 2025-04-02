840f8d356a0dcd85bc6b5fbafc34bb086d3bf024
vynijales <vynijales@gmail.com>
2025-04-02 00:34:29 -0300

    fix: atualizar estilos dos botões para melhorar a consistência visual

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,12 +22,11 @@ const buttonVariants = cva(
           'rounded-md border bg-red-500 p-3 text-white hover:bg-red-600 disabled:bg-red-400',
         'ghost': 'hover:bg-slate-100 hover:text-slate-900',
         'link': 'text-slate-900 underline-offset-4 hover:underline',
-        'header':
-          'bg-brand-header hover:bg-brand-green-500 p-1 text-xl text-white',
+        'header': 'bg-brand-header p-1 text-xl text-white hover:bg-primary-500',
         'new':
-          'hover:border-brand-green-700 hover:text-brand-green-700 border border-transparent bg-white text-green-600 hover:bg-slate-50',
+          'border border-transparent bg-white text-green-600 hover:border-primary-700 hover:bg-slate-50 hover:text-primary-700',
         'table-access':
-          'border-brand-green-400 bg-brand-green-400 hover:border-brand-green-500 hover:bg-brand-green-500 border text-white',
+          'border border-primary-400 bg-primary-400 text-white hover:border-primary-500 hover:bg-primary-500',
         'table-view':
           'border border-blue-500 bg-blue-100 text-xs text-blue-500 shadow hover:border-blue-700 hover:bg-blue-200 hover:text-blue-800',
         'table-add':
@@ -38,8 +37,7 @@ const buttonVariants = cva(
           'border border-red-500 bg-red-100 text-xs text-red-500 shadow hover:border-red-700 hover:bg-red-200 hover:text-red-800',
         'destructive-ghost': 'font-bold text-red-500 hover:bg-red-100',
         'ghost-cancel': 'text-red-500 hover:bg-red-100',
-        'pagination':
-          'bg-brand-green-300/50 hover:bg-brand-green-300 text-slate-900',
+        'pagination': 'bg-primary-300/50 text-slate-900 hover:bg-primary-300',
         'input':
           'w-full rounded-md border border-slate-400 bg-white px-3 py-2 font-normal text-slate-900 disabled:cursor-not-allowed',
       },
